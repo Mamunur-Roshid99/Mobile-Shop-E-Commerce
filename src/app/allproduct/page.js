867f83474c00e07b7e632cbb752e02d68c9563cd
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-09-26 01:12:26 +0600

    .

---
+++ src/app/allproduct/page.js
@@ -0,0 +1,15 @@
+import Navbar from '@/layout/Navbar'
+import NavLink from '@/layout/NavLink'
+import TopNavbar from '@/layout/TopNavbar'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <TopNavbar />
+      <Navbar />
+      <NavLink />
+      dfas
+    </div>
+  )
+}
--- src/page/AboutPage/Index.js
+++ src/page/AboutPage/Index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import TopNav from './TopNav'
 import Provide from './Provide'
+import OurTeam from './OurTeam'
+import CTASection from '../HomePage/CTASection'
 
 export default function Index() {
   return (
@@ -9,6 +11,8 @@ export default function Index() {
       <TopNav />
       <HeroSection />
       <Provide />
+      <OurTeam />
+      <CTASection />
     </div>
   )
 }
